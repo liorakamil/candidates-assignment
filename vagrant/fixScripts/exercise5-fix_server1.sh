1d8cdd9c6eff414cb19f96ce232e19a290eb549a
Your Name <you@example.com>
2019-09-16 10:17:58 +0300

    exercises 1-5

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+su - vagrant -c "mkdir -p /home/vagrant/.ssh && echo \"Host *\" > ~/.ssh/config && echo \" StrictHostKeyChecking no\" >> ~/.ssh/config"
+su - vagrant -c "ssh-keygen -q -t rsa -f ~/.ssh/id_rsa -C '' -N ''"
+
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+su - vagrant -c "mkdir -p /home/vagrant/.ssh && echo \"Host *\" > ~/.ssh/config && echo \" StrictHostKeyChecking no\" >> ~/.ssh/config"
+su - vagrant -c "ssh-keygen -q -t rsa -f ~/.ssh/id_rsa -C '' -N ''"
+apt-get install sshpass
+su - vagrant -c "sshpass -p 'vagrant' scp vagrant@192.168.100.10:~/.ssh/id_rsa.pub ~/id_rsa.pub.server1 && cat ~/id_rsa.pub.server1 >> ~/.ssh/authorized_keys && rm ~/id_rsa.pub.server1"
